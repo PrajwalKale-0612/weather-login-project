0454a3bbb1d7b89ce2bf4f739e7ec805a8d64bc6
PrajwalKale-0612 <kaleprajwal0612@gmail.com>
2026-01-13 12:29:22 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,24 +1,24 @@
-function isStrongPassword(password)
-{
-    if (password.length<8) 
-    {
+function isStrongPassword(password) {
+    if (password.length < 8) return false;
+
+    let upper = 0, lower = 0, number = 0, symbol = 0;
+
+    for (let i = 0; i < password.length; i++) {
+        let ch = password[i];
+
+        if (ch >= 'A' && ch <= 'Z') upper++;
+        else if (ch >= 'a' && ch <= 'z') lower++;
+        else if (ch >= '0' && ch <= '9') number++;
+        else if ("@$!%*?&#".includes(ch)) symbol++;
+    }
+
+    if (upper < 2 || lower < 2 || number < 2 || symbol < 2) {
         return false;
     }
-    let upper=password.match(/[A-Z]/g);
 
-    let lower=password.match(/[a-z]/g);
-
-    let number=password.match(/[0-9]/g);
-
-    let symbol=password.match(/[@$!%*?&#]/g) || [];
-    
-
-    if (upper.length < 2 || lower.length<2 || number.length<2||symbol.length<2) 
-    {
-        return false;
-    }
     return true;
 }
+
 
 function login()
 {
@@ -51,6 +51,7 @@ function logout()
     localStorage.clear();
     window.location.href = "index.html";
 }
+
 
 
 
